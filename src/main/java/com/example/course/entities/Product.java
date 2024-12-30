d86735e827b551af7a620fd3cfdfbc661ba74399
Paulo Hoff <paulohhoff@gmail.com>
2024-12-29 23:40:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,9 +36,8 @@ public class Product implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
-	public Set<Category> getCategories() {
-		return categories;
-	}
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> itens = new HashSet<>();
 
 	public Product() {
 	}
@@ -48,6 +50,10 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -66,6 +72,15 @@ public class Product implements Serializable{
 
 	public String getImgUrl() {
 		return imgUrl;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x: itens) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
